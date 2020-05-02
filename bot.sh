ab01680bdfe032534c370d1ef156d189b39b9e2c
kasan1995 <50731520+kasan1995@users.noreply.github.com>
2020-05-02 13:46:03 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,44 @@
+python main.py +17207298108 doge @Dogecoin_click_bot
+sleep 10
+python main.py +12244004613 doge @Dogecoin_click_bot
+sleep 10
+python main.py +18327534641 doge @Dogecoin_click_bot
+sleep 10
+python main.py +15094858580 doge @Dogecoin_click_bot
+sleep 10
+python main.py +14697458486 doge @Dogecoin_click_bot
+sleep 10
+python main.py +6285779173205 doge @Dogecoin_click_bot
+sleep 10
+python main.py +16066127181 doge @Dogecoin_click_bot
+sleep 10
+python main.py +12242972326 doge @Dogecoin_click_bot
+sleep 10
+python main.py +15154468192 doge @Dogecoin_click_bot
+sleep 10
+python main.py +14012816788 doge @Dogecoin_click_bot
+sleep 10
+python main.py +19563703452 doge @Dogecoin_click_bot
+sleep 10
+python main.py +17207298108 doge @Dogecoin_click_bot
+sleep 10
+python main.py +12244004613 doge @Dogecoin_click_bot
+sleep 10
+python main.py +18327534641 doge @Dogecoin_click_bot
+sleep 10
+python main.py +15094858580 doge @Dogecoin_click_bot
+sleep 10
+python main.py +14697458486 doge @Dogecoin_click_bot
+sleep 10
+python main.py +6285779173205 doge @Dogecoin_click_bot
+sleep 10
+python main.py +16066127181 doge @Dogecoin_click_bot
+sleep 10
+python main.py +12242972326 doge @Dogecoin_click_bot
+sleep 10
+python main.py +15154468192 doge @Dogecoin_click_bot
+sleep 10
+python main.py +14012816788 doge @Dogecoin_click_bot
+sleep 10
+python main.py +19563703452 doge @Dogecoin_click_bot
+sleep 10
